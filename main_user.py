3a55e787e6411d9673b32fb4b913f755b021bd63
C1nth14 <cynthiasusan31@gmail.com>
2022-07-21 17:52:01 +0100

    git commit -m "tried out different countries"

--- main_user.py
+++ main_user.py
@@ -1,6 +1,13 @@
 from single_name import SingleName
 
-country_name = SingleName("Netherlands")
+country_name = SingleName("rwanda")
 
 print(country_name)
 print(country_name.response_data().official)
+print(country_name.response_data().currency)
+print(country_name.response_data().capital)
+print(country_name.response_data().subregion)
+print(country_name.response_data().language)
+print(country_name.response_data().landlocked)
+print(country_name.response_data().car)
+print(country_name.response_data().continent)
--- practice_api.py
+++ practice_api.py
@@ -2,6 +2,6 @@ import requests
 import json
 
 
-countries_fact = requests.get("https://restcountries.com/v3.1/name/netherlands")
+countries_fact = requests.get("https://restcountries.com/v3.1/name/virgin")
 print(countries_fact)
 print(countries_fact.json()[1])
